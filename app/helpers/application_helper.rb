d5e0535d460af1e67389d3ec765bcd3e1b7b6b2a
Ciro Lima <caiazzo14@gmail.com>
2020-02-18 16:25:10 -0300

    Finished exercise o2 rails task manager

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def completed_icon(task)
+    if task.completed?
+      '<i class="far fa-check-square"></i>'
+    else
+      '<i class="far fa-square"></i>'
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # List all tasks
+  get 'tasks', to: 'tasks#index', as: 'tasks'
+
+  # New form
+  get 'tasks/new', to: 'tasks#new', as: 'new'
+
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit'
+
+  # Show single task
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+
+  # Create new task
+  post 'tasks', to: 'tasks#create', as: 'create'
+
+  # Edit task
+  patch 'tasks', to: 'tasks#update', as: 'update'
+
+  # Passar a linguiça numa tarefa
+  delete 'tasks/:id', to: 'tasks#delete', as: 'delete'
 end
